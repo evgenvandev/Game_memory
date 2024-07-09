f600047f4b52e20fe88239eef429c9e7989a9c27
evgenvandev <46090955+evgenvandev@users.noreply.github.com>
2024-07-09 06:26:37 +0000

    02_gh-pages_commit_Duplicated_the_array_of_cards

--- js/script.js
+++ js/script.js
@@ -50,6 +50,9 @@ const cardsArray = [
     }
 ];
 
+// Duplicate array to create a match for each card
+let gameGrid = cardsArray.concat(cardsArray);
+
 // Grab the div with an id of root
 const game = document.getElementById('game');
 
@@ -61,7 +64,7 @@ grid.setAttribute('class', 'grid');
 game.appendChild(grid);
 
 // For each item in the cardsArray array...
-cardsArray.forEach((item => {
+gameGrid.forEach((item => {
     // Create a div
     const card = document.createElement('div');
 
